2ec2bf8398fdc07e191e81445c254f326330fe95
Muhammad Irfanul Haque <mdicmdih@gmail.com>
2018-10-20 16:49:55 +0600

    Added Serial print of LDR value

--- Lesson 3 - LDRRead/LDR+LED.ino
+++ Lesson 3 - LDRRead/LDR+LED.ino
@@ -7,11 +7,14 @@ void setup(){
 
   pinMode(ledPin, OUTPUT);
   pinMode(ldrPin, INPUT);
+  Serial.begin(9600);
 }
 
 void loop(){
 
   int var = analogRead(ldrPin);
+  
+  Serial.println(var);
   
   if(var > threshold)
   {
